6da70d6786309a71648c2c1345ee6520069438ca
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-23 15:22:14 +0900

    Add registerPointPickingCallback example

---
+++ test/ppcallback.jl
@@ -0,0 +1,41 @@
+using PCLCommon
+using PCLIO
+using PCLVisualization
+using Cxx
+
+global clicked_points = PointCloud{PointXYZ}()
+global viewer = PCLVisualizer("point poicking callback test")
+
+function ppcallback(event::cxxt"pcl::visualization::PointPickingEvent&",
+        args::Ptr{Void})
+    if Int(icxx"$(event).getPointIndex();") == -1
+        return
+    end
+
+    global viewer
+    global clicked_points
+
+    p = PointXYZ()
+    icxx"$(event).getPoint($p.x, $p.y, $p.z);"
+    push!(clicked_points, p)
+
+    red_handler = PointCloudColorHandlerCustom(clicked_points, 255, 0, 0)
+    removePointCloud(viewer, id="clicked_points")
+    addPointCloud(viewer, clicked_points, red_handler, id="clicked_points")
+    icxx"""
+    $(viewer.handle)->setPointCloudRenderingProperties(
+        pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, "clicked_points");
+    """
+
+    return nothing::Void
+end
+
+pcd_file = Pkg.dir("PCLIO", "test", "data", "table_scene_lms400.pcd")
+cloud = PointCloud{PointXYZ}(pcd_file);
+white_handler = PointCloudColorHandlerCustom(cloud, 255, 255, 255)
+addPointCloud(viewer, cloud, white_handler, id="cloud")
+
+registerPointPickingCallback(viewer, ppcallback)
+
+spin(viewer)
+close(viewer)
